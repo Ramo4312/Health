7202f943c3555f4a67c52615944c7a9401b61bcf
KrKessedi <pohrglavnoepochta@gmail.com>
2022-10-31 17:55:54 +0600

    asdf

--- src/MainRoutes.js
+++ src/MainRoutes.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Routes, Route } from 'react-router-dom'
+import HomePage from './pages/HomePage'
 import RegistrationPage from './pages/RegistrationPage'
 
 const MainRoutes = () => {
 	return (
 		<Routes>
-			<Route path='/' />
+			<Route path='/' element={<HomePage />} />
 			<Route path='reg' element={<RegistrationPage />} />
 			<Route path='/' />
 			<Route path='/' />
