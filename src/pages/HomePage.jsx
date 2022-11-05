d46cd58ddbeafafb84eb82f90eb6490440d97e4e
KrKessedi <pohrglavnoepochta@gmail.com>
2022-11-05 12:25:30 +0600

    mnc

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
-import SiteBar from '../components/SiteBar'
+import '../styles/HomePage.css'
+import HeaderImage from '../images/zyro-image.png'
+import { motion } from 'framer-motion'
+import '../styles/adaptive/HomePage-adaptive.css'
 
 const HomePage = () => {
-	return <h1>Home Page</h1>
+	return (
+		<motion.div
+			className='home-container'
+			initial={{ width: 0, opacity: 0 }}
+			animate={{ width: '100%', opacity: 1 }}
+			exit={{ width: window.innerWidth, opacity: 0 }}
+		>
+			<div className='home-container__left'>
+				<h1 className='home-container__left-text-big'>
+					<span className='logo-color'>Health</span> решение для всех ваших
+					проблем со здоровьем
+				</h1>
+				<p className='home-container__left-text-small'>
+					По вашим характеристикам мы подбираем вам блюдо, тренировки,
+					расписание
+				</p>
+				<button className='home-container__left-button def-block2'>
+					Начать
+				</button>
+			</div>
+			<div className='home-container-right'>
+				<img src={HeaderImage} alt='error' className='home-container-img' />
+			</div>
+			<button className='home-container__left-button def-none2'>Начать</button>
+		</motion.div>
+	)
 }
 
 export default HomePage
