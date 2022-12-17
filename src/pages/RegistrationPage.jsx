2cbca2e1a59b7328a568759eebf3bec6e4160b7a
Ramo4312 <ramoalikulov77@gmail.com>
2022-12-17 21:45:34 +0600

    added register page adaptive and register logic

--- src/pages/RegistrationPage.jsx
+++ src/pages/RegistrationPage.jsx
@@ -1,49 +1,34 @@
 import React, { useState } from 'react'
 import '../styles/RegistrationPage.css'
-import '../styles/adaptive/RegisterPage-adaptive.css'
-import { createTheme, ThemeProvider } from '@mui/material/styles'
 import { useNavigate } from 'react-router-dom'
-import { motion } from 'framer-motion'
 import { useAuth } from '../contexts/authContext'
-
-const lightTheme = createTheme({
-	palette: {
-		mode: 'dark',
-		primary: {
-			main: '#fff',
-		},
-	},
-})
 
 const RegistrationPage = () => {
 	const { registration } = useAuth()
 
 	let navigate = useNavigate()
 
-	const [name, setName] = useState('')
-	const [surname, setSurname] = useState('')
 	const [email, setEmail] = useState('')
+	const [username, setNickname] = useState('')
+	const [sex, setSex] = useState('')
+	const [age, setAge] = useState('')
 	const [password, setPassword] = useState('')
 	const [password2, setPassword2] = useState('')
-	const [username, setNickname] = useState('')
 
 	function registerSystem() {
 		if (
-			// !username.trim() ||
-			// !surname.trim() ||
 			!username.trim() ||
 			!email.trim() ||
 			!password.trim() ||
-			!password2.trim()
+			!password2.trim() ||
+			!age.trim()
 		) {
 			alert('Some inputs are empty')
 			return
 		}
-		registration(username, email, password, password2)
-		navigate('/login')
+		registration(email, username, sex, +age, password, password2, navigate)
 	}
 
-	// console.log(surname)
 	return (
 		<div className='register-form-page'>
 			<div className='register-form-page-background-1'></div>
@@ -58,7 +43,7 @@ const RegistrationPage = () => {
 				<div className='input-desc'>Имя</div>
 				<input
 					value={username}
-					onChange={(e) => setNickname(e.target.value)}
+					onChange={e => setNickname(e.target.value)}
 					type='text'
 					className='nickname-inp'
 				/>
@@ -66,24 +51,29 @@ const RegistrationPage = () => {
 				<div className='input-desc'>Email</div>
 				<input
 					value={email}
-					onChange={(e) => setEmail(e.target.value)}
+					onChange={e => setEmail(e.target.value)}
 					type='text'
 					className='reg_email-inp'
 				/>
 
 				<div className='age-gender-block'>
 					<span>Возраст</span>
-					<input />
-					<select name='Пол' placeholder='Пол'>
-						<option>муж</option>
-						<option>жен</option>
+					<input value={age} onChange={e => setAge(e.target.value)} />
+					<select
+						name='Пол'
+						placeholder='Пол'
+						onChange={e => setSex(e.target.value)}
+					>
+						<option value='gender'>Пол</option>
+						<option value='male'>муж</option>
+						<option value='female'>жен</option>
 					</select>
 				</div>
 
 				<div className='input-desc'>Пароль</div>
 				<input
 					value={password}
-					onChange={(e) => setPassword(e.target.value)}
+					onChange={e => setPassword(e.target.value)}
 					type='text'
 					className='reg_password-inp'
 				/>
@@ -91,7 +81,7 @@ const RegistrationPage = () => {
 				<div className='input-desc'>Повторите пароль</div>
 				<input
 					value={password2}
-					onChange={(e) => setPassword2(e.target.value)}
+					onChange={e => setPassword2(e.target.value)}
 					type='text'
 					className='passwordConf-input'
 				/>
@@ -100,10 +90,14 @@ const RegistrationPage = () => {
 					Регистрация
 				</button>
 
-				<button className='register-btn-log-in' onClick={registerSystem}>
+				<button
+					className='register-btn-log-in'
+					onClick={() => {
+						navigate('/login')
+					}}
+				>
 					Вход
 				</button>
-				
 			</div>
 		</div>
 	)
