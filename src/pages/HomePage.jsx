376eabd5aae0d8c5d58c3cf2fecb0aff79134316
Ramo4312 <ramoalikulov77@gmail.com>
2022-12-17 19:37:28 +0600

    commit

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-// import '../styles/HomePage.css'
 import '../styles/HomePage.css'
 import HeaderImage from '../images/Healthy lifestyle-bro (1) 1.svg'
 import { motion } from 'framer-motion'
@@ -10,22 +9,25 @@ const HomePage = () => {
 	const navigate = useNavigate()
 
 	return (
-		<motion.div
-			initial={{ opacity: 0, translateX: -50 }}
-			animate={{ opacity: 1, translateX: 0 }}
-			transition={{ duration: 0.3, delay: 0.5 }}
-		>
-			<div className='home-container'>
+		<>
+			<motion.div
+				// style={{ overflowY: 'hidden', width: '100%' }}
+				initial={{ opacity: 0, translateX: -50 }}
+				animate={{ opacity: 1, translateX: 0 }}
+				transition={{ duration: 0.3, delay: 0.5 }}
+			>
+				{/* <div className='home-container'>
 				<div className='home-container__left'>
 					<h1 className='home-container__left-text-big'>
 						<span className='logo-color'>HEALTH</span>-Расчет калорий
-						употребляемые вам в день.
+						употребляемые вами в день.
 					</h1>
 					<p className='home-container__left-text-small'>
-						Мы вам подберем индвидиуальные рецепты для идеальной талий
+						Мы вам поможем найти свой идеальный вес,рассчитать калории и
+						подобрать рецепты правильного питания.
 					</p>
 					<motion.button
-						whileHover={{ scale: 1.1 }}
+						whileHover={{ translateY: '-3px' }}
 						className='home-container__left-button def-block2'
 						onClick={() => navigate('/login')}
 					>
@@ -34,17 +36,17 @@ const HomePage = () => {
 				</div>
 				<div className='home-container-right'>
 					<img src={HeaderImage} alt='error' className='home-container-img' />
-					{/* <motion.button
-					whileHover={{ scale: 1.1 }}
-					className='home-container__left-button def-none2'
-				>
-					Начать
-				</motion.button> */}
 				</div>
 			</div>
 			<div className='bg-ellipse-1'></div>
-			<div className='bg-ellipse-2'></div>
-		</motion.div>
+			<div className='bg-ellipse-2'></div> */}
+			</motion.div>
+			<div className='animation-background'>
+				<div className='animation-block-1'></div>
+				<div className='animation-block-2'></div>
+				<div className='animation-block-3'></div>
+			</div>
+		</>
 	)
 }
 
