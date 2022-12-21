0839471b27d9357c34758ecdde9f5de5de981385
Bakyt <bakyt.gaipov.kk@gmail.com>
2022-12-21 22:23:39 +0600

    create recipesPage

---
+++ src/pages/RecipesPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../styles/RecipesPage.css';
+
+const RecipesPage = () => {
+	return (
+		<div className="main-container">
+			
+		</div>
+	)
+}
+
+export default RecipesPage;
